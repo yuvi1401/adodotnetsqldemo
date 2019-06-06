c755949df87e7d21e8847b9832b6bbda7bd6ab0a
yuvi1401 <ykaero@gmail.com>
2019-06-06 06:46:07 +0100

    ExecuteReader, Execute scaler and ExecuteNonQuery  examples

--- SqlDemoConnection/WebForm2.aspx.cs
+++ SqlDemoConnection/WebForm2.aspx.cs
@@ -18,12 +18,59 @@ namespace SqlDemoConnection
             //string CS = "data source=.; database = Sample;  integrated security = SSPI";
             using (SqlConnection con = new SqlConnection(CS))
             {
-                SqlCommand cmd = new SqlCommand("Select * from ProductsTable", con);
+                SqlCommand cmd = new SqlCommand("SELECT * FROM ProductsTable", con);
                 con.Open();
 
                 GridView1.DataSource = cmd.ExecuteReader();
                 GridView1.DataBind();
-                // con.Close();
+
+                //SqlCommand cmd1 = new SqlCommand();
+                //cmd1.CommandText = "SELECT COUNT(ProductId) FROM ProductsTable";
+                //cmd1.Connection = con;
+                con.Close();
+
+                SqlCommand cmd1 = new SqlCommand("SELECT COUNT(ProductId) FROM ProductsTable", con);
+                con.Open();
+                int TotalRows = (int)cmd1.ExecuteScalar();
+
+
+                Response.Write("Total Rows = " + TotalRows.ToString() + "<br/>");
+
+                con.Close();
+
+                //SqlCommand cmd2 = new SqlCommand("INSERT into ProductsTable values (6, 'keyboard', 20, 7)", con);
+                //con.Open();
+
+                //int TotalRowsAffected = (int)cmd2.ExecuteNonQuery();
+
+
+                //Response.Write("Total Rows Affected = " + TotalRowsAffected.ToString());
+                //con.Close();
+
+                SqlCommand cmd3 = new SqlCommand();
+                cmd3.Connection = con;
+                con.Open();
+
+               // GridView1.DataSource = cmd3.ExecuteReader();
+                //GridView1.DataBind();
+
+                // using same command object
+
+                cmd3.CommandText = "DELETE from ProductsTable WHERE ProductId = 6";
+
+               int TotalRowsAffected = (int)cmd3.ExecuteNonQuery();
+                Response.Write("Total Rows Deleted = " + TotalRowsAffected.ToString() + "<br/>");
+
+                cmd3.CommandText = "INSERT into ProductsTable values (6, 'keyboard', 20, 7)";
+
+                 TotalRowsAffected = (int)cmd3.ExecuteNonQuery();
+                Response.Write("Total Rows Inserted = " + TotalRowsAffected.ToString() + "<br/>");
+          
+
+                //GridView1.DataSource = cmd3.ExecuteReader();
+                //GridView1.DataBind();
+
+
             }
         }
 
